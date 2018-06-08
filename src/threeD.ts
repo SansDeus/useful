3807dfb052f027d5232e8fb52ceb630d56ea63dc
SansDeus <theaaronedwards+github@gmail.com>
2018-06-08 14:14:14 -0700

    simplified calls

--- src/threeD.ts
+++ src/threeD.ts
@@ -1,8 +1,8 @@
 import Decasteljau from './decasteljau';
-
 export default (points: { x: number, y: number, z: number }[], time: number) => {
-	const xArray = Decasteljau.calculate(points.map((p) => p.x), time);
-	const yArray = Decasteljau.calculate(points.map((p) => p.y), time);
-	const sizeArray = Decasteljau.calculate(points.map((p) => p.z), time);
-	return { x: xArray, y: yArray, z: sizeArray };
+	return { 
+		x: Decasteljau.calculate(points.map((p) => p.x), time),
+		y: Decasteljau.calculate(points.map((p) => p.y), time),
+		z: Decasteljau.calculate(points.map((p) => p.z), time)
+	};
 }
