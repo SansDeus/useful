42ca5b455954e90443f68ef04e37a38813911d4b
SansDeus <33769513+SansDeus@users.noreply.github.com>
2019-12-15 01:43:35 -0600

    Update decasteljau.ts
    Changed time to percent. A better indication of what is actually used.

--- src/decasteljau.ts
+++ src/decasteljau.ts
@@ -1,16 +1,16 @@
 import lerp from "./lerp";
 const Decasteljau = {
-	calculate (points: number[], time: number) {
+	calculate (points: number[], percent: number) {
 		let result : number[] = [points.length - 1];
 		for (let i = 0, j = points.length - 1; i < j; i++) {
-			result[i] = lerp(points[i], points[i + 1], time);
+			result[i] = lerp(points[i], points[i + 1], percent);
 		}
 		if (result.length === 1) {
 			const value = result[0];
 			result = null;
 			return value;
 		}
-		return this.calculate(result, time);
+		return this.calculate(result, percent);
 	}
 }
 export default Decasteljau;
