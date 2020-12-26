273fa5a9338e3905fe19dbc51734d8208501fe91
SansDeus <theaaronedwards+github@gmail.com>
2020-12-26 13:44:49 -0800

    Update decasteljau.ts
    Previous change destroyed the original array. Made it non-destructive again.

--- src/decasteljau.ts
+++ src/decasteljau.ts
@@ -1,7 +1,6 @@
 import { Lerp } from "./lerp";
 export const Decasteljau = (points: number[], percent: number): number => {
-	let result = points.map((n, i) => {
-		return i !== points.length ? Lerp(n, points[i + 1], percent) : NaN;
-	}).slice(0, points.length - 1);
-	return result.length === 1 ? result[0] : Decasteljau(result, percent);
+	if (points.length === 1) return points[0];
+	const results = [...points].map((p, i) => i !== points.length ? Lerp(points[i], points[i + 1], percent) : NaN).slice(0, points.length - 1);
+	return Decasteljau(results, percent);
 };
