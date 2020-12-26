11c7b7c69c77f79e6a71456b260e2a09d9041132
SansDeus <33769513+SansDeus@users.noreply.github.com>
2020-12-26 09:37:23 -0800

    Removed typecasting

--- src/decasteljau.ts
+++ src/decasteljau.ts
@@ -1,7 +1,7 @@
 import { Lerp } from "./lerp";
 export const Decasteljau = (points: number[], percent: number): number => {
 	let result = points.map((n, i) => {
-		return i !== points.length ? Lerp(n as number, points[i + 1] as number, percent) : NaN;
+		return i !== points.length ? Lerp(n, points[i + 1], percent) : NaN;
 	}).slice(0, points.length - 1);
 	return result.length === 1 ? result[0] : Decasteljau(result, percent);
 };
