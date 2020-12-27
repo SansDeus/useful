319aacb8091bd60ddcd93a278cf9435f2adf4dcd
SansDeus <theaaronedwards+github@gmail.com>
2020-12-26 18:18:15 -0800

    Update decasteljau.ts
    Trade size for performance.

--- src/decasteljau.ts
+++ src/decasteljau.ts
@@ -1,6 +1,11 @@
 import { Lerp } from "./lerp";
 export const Decasteljau = (points: number[], percent: number): number => {
 	if (points.length === 1) return points[0];
-	const results = [...points].map((p, i) => i !== points.length ? Lerp(points[i], points[i + 1], percent) : NaN).slice(0, points.length - 1);
-	return Decasteljau(results, percent);
+	const results = [...points];
+	const calc = (p: number[]): number => {
+		for(let i = 0, e = p.length - 1; i < e; i++) results[i] = Lerp(p[i], p[i + 1], percent);
+		results.pop();
+		return results.length === 1 ? results[0] : calc(results);
+	};
+	return calc(results);
 };
